7c845e7fb2ea5d5fbd227b6a28bf0d9625f48690
MUYUM <krasovec.n@gmail.com>
2018-01-25 20:45:50 +0100

    Main

---
+++ SIGMA_API/src/main/java/Server/Server.java
@@ -0,0 +1,20 @@
+package Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Server
+{
+    // Server [ TomCat ]
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Server.class, args);
+    }
+}
