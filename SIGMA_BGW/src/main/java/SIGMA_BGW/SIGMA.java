cf9671bfa7acbcc8e3a0ad9e92da173ae8192492
MUYUM <krasovec.n@gmail.com>
2018-01-26 04:47:15 +0100

    Main

--- SIGMA_BGW/src/main/java/SIGMA_BGW/SIGMA.java
+++ SIGMA_BGW/src/main/java/SIGMA_BGW/SIGMA.java
@@ -1,10 +1,15 @@
 package SIGMA_BGW;
 
+import SIGMA_BGW.PCS.OCS_PCS_DTA;
+
 public class SIGMA
 {
     public static void main(String[] args)
     {
         System.out.println("SigmaBGW");
         System.out.println("=========================");
+        
+        OCS_PCS_DTA PCS = new OCS_PCS_DTA("");
+        PCS.start();
     }
 }
